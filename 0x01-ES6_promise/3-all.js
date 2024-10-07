fc3929a3c47b61dcd74beb8793d6dda88071bb49
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-10-07 03:22:06 +0000

    Update solution 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handlProfileSignup() {
-	return Promise
-		.all([uploadPhoto(), createUser()])
-		.then((results) => console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`))
-		.catch(() => console.log('Signup system offline'));
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((results) => console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
