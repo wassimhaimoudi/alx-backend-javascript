6a5e2cf73e6e89963673e305d16dbf3901f3e227
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-10-07 03:14:48 +0000

    Add solution 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,5 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handlProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((results) => console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`)).catch(() => console.log('Signup system offline'));
+}
