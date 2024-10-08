6e37fe2cd226e7402a40cf541efadc7413c34f88
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-10-08 11:24:26 +0000

    Update solution 7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload])
-    .then((result) => console.log(result));
+  return Promise.race([chinaDownload, USDownload])
+    .then((result) => result);
 }
