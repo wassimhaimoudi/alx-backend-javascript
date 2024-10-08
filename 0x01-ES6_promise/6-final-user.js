28046faada0890bb1f5eefe5d33b461db3b4209d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-10-08 11:07:21 +0000

    Add solution 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results);
+    .then((results) => results.map((x) => ({ status: x.status,
+    value: x.status === "fulfilled" ? x.status : String(x.reason)})));
 }
