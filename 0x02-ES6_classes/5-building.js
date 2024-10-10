a0b0d8bef2a9a6ba861713070aaa35468d392eee
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-10-10 19:50:57 +0000

    add solution 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (!(typeof newSqft === 'number')) {
+      throw new TypeError('sqft must be a number');
+    }
+
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
