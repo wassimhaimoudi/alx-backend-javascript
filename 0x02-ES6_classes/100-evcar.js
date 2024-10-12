339098ce0b5a642a30d6a391f2a342d317656c3d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-10-12 01:04:30 +0000

    add solution 11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(newRange) {
+    this._range = newRange;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+    return new Species();
+  }
+}
