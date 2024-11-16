ef4f8f151fa5c0c4016bef6e2b89b47b30c31c76
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-11-16 14:46:32 +0000

    add task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  let resultString = '';
+  let flag = 1;
+
+  for (const stringElement of set) {
+    if (stringElement.startsWith(startString)) {
+      if (!flag) {
+        resultString += '-';
+      }
+
+      resultString += stringElement.slice(startString.length);
+      flag = 0;
+    }
+  }
+
+  console.log(resultString);
+}
