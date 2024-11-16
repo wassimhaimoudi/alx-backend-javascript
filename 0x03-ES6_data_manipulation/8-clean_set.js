f8119548e648439bd62c7b48c68c7a5b45d56559
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-11-16 16:21:12 +0000

    update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,11 @@
 export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) return '';
 
-  let resultString = '';
-  let flag = 0;
+  // Filter elements in the set that start with the startString and slice off the prefix
+  const result = [...set]
+    .filter((item) => typeof item === 'string' && item.startsWith(startString))
+    .map((item) => item.slice(startString.length));
 
-  for (const stringElement of set) {
-    if (typeof stringElement === 'string' && stringElement.startsWith(startString)) {
-      if (flag) {
-        resultString += '-';
-      }
-
-      resultString += stringElement.split(startString)[1];
-      flag = 1;
-    }
-  }
-
-  return resultString;
+  // Join the resulting array with hyphens
+  return result.join('-');
 }
